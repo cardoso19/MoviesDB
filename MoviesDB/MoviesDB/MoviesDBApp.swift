7375033411befa2b4b76c9fd559b969e35e5ca02
Matheus Kuhn <matheuscardosokuhn@hotmail.com>
2024-12-23 19:55:28 +0100

    Add base project

---
+++ MoviesDB/MoviesDB/MoviesDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDBApp.swift
+//  MoviesDB
+//
+//  Created by Matheus Kuhn on 23.12.24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesDB/MoviesDBTests/MoviesDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDBTests.swift
+//  MoviesDBTests
+//
+//  Created by Matheus Kuhn on 23.12.24.
+//
+
+import Testing
+@testable import MoviesDB
+
+struct MoviesDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
